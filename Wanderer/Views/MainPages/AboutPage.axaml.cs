6b9e3bd0af96f36bf6b30f6133b6c7edaa3d6b8e
lrs2187 <liurongshuo2022@outlook.com>
2026-04-05 16:03:03 +0800

    feat: 新的设置界面

--- Wanderer/Views/MainPages/AboutPage.axaml.cs
+++ Wanderer/Views/MainPages/AboutPage.axaml.cs
@@ -1,4 +1,7 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using System.Diagnostics;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.Input;
 using Wanderer.Attributes;
 
 namespace Wanderer.Views.MainPages;
@@ -8,6 +11,33 @@ public partial class AboutPage : UserControl
 {
     public AboutPage()
     {
+        DataContext = this;
         InitializeComponent();
+    }
+
+    [RelayCommand]
+    private void OpenLink(string url)
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            Process.Start(new ProcessStartInfo(url)
+            {
+                UseShellExecute = true
+            });
+        }
+        else if (OperatingSystem.IsLinux())
+        {
+            Process.Start(new ProcessStartInfo("xdg-open", url)
+            {
+                UseShellExecute = false
+            });
+        }
+        else if (OperatingSystem.IsMacOS())
+        {
+            Process.Start(new ProcessStartInfo("open", url)
+            {
+                UseShellExecute = false
+            });
+        }
     }
 }
